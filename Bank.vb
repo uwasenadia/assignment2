7ee6cb146416ccd6ef942f2923975768cc3f0d97
uwasenadia <108819723+uwasenadia@users.noreply.github.com>
2022-07-14 09:01:01 -0700

    Add files via upload

---
+++ Bank.vb
@@ -0,0 +1,20 @@
+﻿Public Class Bank
+    Public AccountNumber As String
+    Public OwnerName As String
+    Public OwnerAddress As String
+    Public OwnerEmail As String
+    Public Balance As Decimal
+    Public Overrides Function ToString() As String
+        Return AccountNumber & vbTab & OwnerName & vbTab & OwnerAddress & vbTab & OwnerEmail & vbTab & Balance
+    End Function
+    Sub New(ACCOUNT As String, NAME As String, ADDRESS As String, EMAIL As String, BALANCE As Decimal)
+        Me.AccountNumber = ACCOUNT
+        Me.OwnerName = NAME
+        Me.OwnerAddress = ADDRESS
+        Me.OwnerEmail = EMAIL
+        Me.Balance = BALANCE
+    End Sub
+    Sub New()
+
+    End Sub
+End Class
---
+++ Module1.vb
@@ -0,0 +1,51 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim choice As String
+        choice = ""
+        Do
+            Console.WriteLine("1. CREATE ACCOUNT" & vbNewLine & "2. DISPLAY ACCOUNT" & vbNewLine & vbNewLine & "CHOOSE OPTION")
+            choice = Console.ReadLine()
+            Select Case choice
+                Case "1"
+                    CreateAccounts()
+                Case "2"
+                    DisplayAccounts()
+                Case Else
+                    Console.WriteLine("INVALID CHOICE")
+            End Select
+
+        Loop While choice <> ""
+        Console.ReadLine()
+    End Sub
+    Dim ACCLIST As List(Of Bank) = New List(Of Bank)
+    Sub CreateAccounts()
+        Dim AccNo, Name, Addr, Email As String
+        Dim balance As Decimal
+        AccNo = ""
+        Do
+            Console.WriteLine("ENTER ACCOUNT NUMBER")
+            AccNo = Console.ReadLine()
+            If AccNo = "" Then
+                Exit Do
+            End If
+            Console.WriteLine("ENTER  NAME")
+            Name = Console.ReadLine()
+            Console.WriteLine("ENTER  ADDRESS")
+            Addr = Console.ReadLine()
+            Console.WriteLine("ENTER EMAIL")
+            Email = Console.ReadLine()
+            Console.WriteLine("ENTER BALANCE")
+            balance = Console.ReadLine()
+            ACCLIST.Add(New Bank(AccNo, Name, Addr, Email, balance))
+        Loop While AccNo <> ""
+
+    End Sub
+    Sub DisplayAccounts()
+        For Each account In ACCLIST
+            Console.WriteLine(account)
+        Next
+        Console.ReadLine()
+    End Sub
+
+End Module
